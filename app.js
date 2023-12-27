881f6855505d6c18ef248e37279e91a093f95520
chrissha balbin <cbalbin_20ur1110@psu.edu.ph>
2023-12-27 13:48:33 +0800

    first push

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const session = require('express-session');
+
+
+app.use(session({
+    secret: 'abc',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log('Connection Error');
+    } else {
+        console.log(`Listening to port ${port} `);
+    }
+})
+
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/', require('./routes/route.js'));
---
+++ controller/controller.js
@@ -0,0 +1,5 @@
+const con = require("../database/connection.js");
+const flash = require("express-flash");
+const argon2 = require("argon2");
+
+
